350bd3fcef898adb7b7898b27af9b64344670f2a
Gaudzyte <gaudzyteurte@gmail.com>
2025-11-23 19:37:43 +0200

    Create zmogus.h

---
+++ zmogus.h
@@ -0,0 +1,23 @@
+#ifndef ZMOGUS_H
+#define ZMOGUS_H
+
+#include <string>
+
+class Zmogus {
+protected:
+    std::string vardas_;
+    std::string pavarde_;
+
+public:
+    Zmogus() = default;
+
+    Zmogus(const std::string& vardas, const std::string& pavarde)
+        : vardas_(vardas), pavarde_(pavarde) {}
+
+    virtual ~Zmogus() = default;
+
+    virtual std::string vardas() const = 0;
+    virtual std::string pavarde() const = 0;
+};
+
+#endif
